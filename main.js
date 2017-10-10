d0a4fd2964952ddfdc4b448b7dcdb99997bb960b
Kai Brabo <kaibrabo@gmail.com>
2017-10-10 11:51:38 -0700

    create table, paddles and ball

--- main.js
+++ main.js
@@ -2,97 +2,94 @@
 var canvas = document.getElementById('a');
 var context = canvas.getContext('2d');
 
-var pi = Math.PI;
-var radius;
-
-var border = function(){
-    context.lineWidth = 5;
-    context.strokeStyle = 'black';
-    context.stroke();
+// Pong Table
+function Table(w, h) {
+    this.w = w;
+    this.h = h;
 }
 
-// Pong Table
-context.beginPath();
-context.rect(10, 50, 580, 340);
-context.fillStyle = '#444';
-context.fill();
-border();
+Table.prototype.render = function(context) {
+    var radius = 80;
+    var border = function(){
+        context.lineWidth = 5;
+        context.strokeStyle = 'black';
+        context.stroke();
+    }
 
-//  table decals
-radius = 80;
-// right side
-var sA = pi * 0.5;
-var eA = pi * 1.5;
-context.beginPath();
-context.arc(588, 220, radius, sA, eA, false);
-border();
-// left side
-sA = pi * 1.5;
-eA = pi * 0.5;
-context.beginPath();
-context.arc(12, 220, radius, sA, eA, false);
-border();
+    context.beginPath();
+    context.rect(0, 0, this.w, this.h);
+    context.fillStyle = '#444';
+    context.fill();
+    border();
+
+    //  table decals
+    // right side
+    var sA = Math.PI * 0.5;
+    var eA = Math.PI * 1.5;
+    context.beginPath();
+    context.arc(this.w, this.h / 2, radius, sA, eA, false);
+    border();
+    // left side
+    sA = Math.PI * 1.5;
+    eA = Math.PI * 0.5;
+    context.beginPath();
+    context.arc(0, this.h / 2, radius, sA, eA, false);
+    border();
+};
 
 // Paddle constructor
 function Paddle(x, y, w, h, c){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+}
+
+Paddle.prototype.render = function(context) {
     context.beginPath();
-    context.rect(x, y, w, h);
-    context.fillStyle = c;
+    context.rect(this.x, this.y, this.w, this.h);
+    context.fillStyle = this.c;
     context.fill();
     context.lineWidth = 5;
     context.strokeStyle = 'black';
     context.stroke();
-}
-
-// Player Paddle
-var player = new Paddle(565, 170, 15, 90, 'blue');
-
-// Computer Paddle
-var computer = new Paddle(20, 170, 15, 90, 'red');
+};
 
 // Ball
-var x = canvas.width / 2;
-var y = canvas.height / 2;
-radius = 15;
+function Ball(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = 15;
+    this.sA = 0;
+    this.eA = Math.PI * 2;
+    this.cC = true;
+};
 
-var ball = new Ball(x, y, radius, 0, pi*2, false);
-
-function Ball(x, y, radius, startAngle, endAngle, counterClockwise){
+Ball.prototype.render = function(context) {
     context.beginPath();
-    context.arc(x - 20, y, radius, startAngle, endAngle, counterClockwise);
+    context.arc(this.x, this.y, this.radius, this.sA, this.eA, this.cC);
     context.fillStyle = 'gray';
     context.fill();
     context.lineWidth = 5;
     context.strokeStyle = 'black';
     context.stroke();
-}
+};
 
-// function Ball(){
-//     this.x = canvas / 2;
-//     this.y = canvas / 2;
-//     this.radius = 15;
-//     this.sA = 0;
-//     this.eA = pi * 2;
-//     this.cC = true;
-//     this.render = function(){
-//         context.beginPath();
-//         context.arc(this.x, this.y, this.radius, this.sA, this.eA, this.cC);
-//         context.fillStyle = 'gray';
-//         context.fill();
-//         context.lineWidth = 5;
-//         context.strokeStyle = 'black';
-//         context.stroke();
-//     };
-// };
-//
-//
-// // Instantiated Objects
-// var ball = new Ball();
-//
-// function renderAll(){
-//     ball.render();
-// };
-//
-// window.onload = function(){
-//     renderAll();
-// };
+// Instantiated Objects
+var ball = new Ball(canvas.width / 2, canvas.height / 2);
+var player = new Paddle(canvas.width - (20 + 15), (canvas.height - 90) / 2, 15, 90, 'blue');
+var computer = new Paddle(20, (canvas.height - 90) / 2, 15, 90, 'red');
+var table = new Table(canvas.width, canvas.height);
+
+
+function renderAll(){
+    table.render(context);
+    ball.render(context);
+    player.render(context);
+    computer.render(context);
+};
+
+window.onload = function(){
+    renderAll();
+};
