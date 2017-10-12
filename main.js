35c69a3e3ff7cea8aa819b5769bfc108c834e28c
Kai Brabo <kaibrabo@gmail.com>
2017-10-12 16:54:03 -0700

    detect collisions

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ function Table(w, h) {
     this.h = h;
 }
 
+Table.prototype.resetRound = function() {
+    ball = new Ball(canvas.width / 2, canvas.height / 2);
+}
+
 Table.prototype.render = function(context) {
     var radius = 80;
     var border = function(){
@@ -32,6 +36,7 @@ Table.prototype.render = function(context) {
     context.beginPath();
     context.arc(this.w, this.h / 2, radius, sA, eA, false);
     border();
+
     // left side
     sA = Math.PI * 1.5;
     eA = Math.PI * 0.5;
@@ -47,7 +52,7 @@ function Paddle(x, y, w, h, c){
     this.w = w;
     this.h = h;
     this.c = c;
-    this.speed = 10;
+    this.speed = 20;
 }
 
 Paddle.prototype.render = function(context) {
@@ -67,24 +72,45 @@ Paddle.prototype.render = function(context) {
 function onKeyDown(e) {
     // up arrow key
     if (e.keyCode == 38) {
-        if (player.y >= 10) {
-            player.move(-10);
+        if (player.y >= 20) {
+            player.move(-20);
         }
     }
 
     // down arrow key
     if (e.keyCode == 40) {
-        if (player.y <= 300) {
-            player.move(10);
+        if (player.y <= 290) {
+            player.move(20);
         }
+    }
+
+    if (e.keyCode == 13) {
+        ball_reset();
     }
 }
 
+// paddle event
 function addKeyEvents() {
     window.addEventListener('keydown', onKeyDown, true);
 }
 
 // Ball
+function Velocity() {
+    this.x = 0;
+    this.y = 0;
+}
+
+Velocity.prototype.randomize = function() {
+    // direction of player paddle
+    this.x = -1;
+    this.y = 0;
+
+    // to randomize direction
+    // var direction = 2 * Math.PI * Math.random();
+    // this.x = Math.cos(direction);
+    // this.y = Math.sin(direction);
+};
+
 function Ball(x, y) {
     this.x = x;
     this.y = y;
@@ -92,6 +118,8 @@ function Ball(x, y) {
     this.sA = 0;
     this.eA = Math.PI * 2;
     this.cC = true;
+    this.v = new Velocity();
+    this.v.randomize();
 };
 
 Ball.prototype.render = function(context) {
@@ -102,6 +130,49 @@ Ball.prototype.render = function(context) {
     context.lineWidth = 5;
     context.strokeStyle = 'black';
     context.stroke();
+};
+
+Ball.prototype.step = function(table) {
+        var player_front = player.x;
+        var player_top = player.y;
+        var player_bottom = player.y + player.h;
+        var computer_front = computer.x + computer.w;
+        var computer_top = computer.y;
+        var computer_bottom = computer.y + computer.h;
+        this.x += this.v.x;
+        this.y += this.v.y;
+        this.top = this.y - 5;
+        this.bottom = this.y + 5;
+        this.left = this.x - 5;
+        this.right = this.x + 5;
+
+        // hits wall
+        if (this.y <= this.radius || this.y >= table.h - this.radius) {
+            this.v.y *= -1;
+        }
+
+        if (this.x <= -this.radius || this.x >= table.w + this.radius) {
+            table.resetRound();
+
+        }
+
+        // hits player paddle
+        if (this.x + this.radius >= player_front &&
+            player_bottom >= this.y + this.radius &&
+            this.y - this.radius >= player_top) {
+            this.v.x *= -1;
+        }
+
+        // hits computer paddle
+        if (this.x - this.radius <= computer_front &&
+            computer_bottom >= this.y + this.radius &&
+            computer_top <= this.y - this.radius) {
+            this.v.x *= -1;
+        }
+};
+
+var ball_reset = function() {
+    location.reload();
 };
 
 // Instantiated Objects
@@ -119,8 +190,10 @@ function renderAll(){
 };
 
 var step = function() {
-    renderAll();
     animate(step);
+    ball.step(table);
+
+    renderAll();
 };
 
 window.onload = function(){
